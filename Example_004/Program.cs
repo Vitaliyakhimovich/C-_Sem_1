8bd7c36906673f9425658ef6b4ba8a1fe85e77a2
«Vitaly» <vitalik.yakhimovich@mail.ru>
2023-03-20 22:43:58 +0300

    add Example 004

---
+++ Example_004/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.WriteLine("Введите день недели числом");
+int number = int.Parse(Console.ReadLine());
+
+if(number <= 7 && number >= 1)
+{
+    if (number == 1)
+    {
+        Console.WriteLine("Вы ввели Понедельник");
+    }
+    else
+    {
+        if (number == 2)
+        {
+            Console.WriteLine("Вы ввели Вторник");
+        }
+        else
+        {
+           if (number == 3) 
+           {
+            Console.WriteLine("Вы ввели Среду");
+           }
+           else
+           {
+            if (number == 4)
+            {
+                Console.WriteLine("Вы ввели Четверг");
+            }
+            else 
+            {
+               if (number == 5)
+               {
+                Console.WriteLine("Вы ввели Пятницу");
+               } 
+               else
+               {
+                if (number == 6)
+                {
+                 Console.WriteLine("Вы ввели Субботу");
+                }
+                else
+                {
+                Console.WriteLine("Вы ввели Воскресенье");
+                }
+               }
+            }
+           }
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Нет такого дня недели");
+}
