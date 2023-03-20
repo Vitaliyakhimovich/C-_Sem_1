106c8aba5c8f34d23b2a2b848e66deaca699c860
«Vitaly» <vitalik.yakhimovich@mail.ru>
2023-03-20 21:50:59 +0300

    add Example 001

---
+++ Example_001/Program.cs
@@ -0,0 +1,8 @@
+﻿// Введеите число и возведите его в квадрат
+
+Console.WriteLine("Введите целое число");
+int a = int.Parse(Console.ReadLine());
+
+int result = a * a;
+
+Console.WriteLine($"Квадрат введеного числа = {result}");
