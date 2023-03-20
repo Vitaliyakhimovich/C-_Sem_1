0c65e0052c9ec3cd703f31ef227a0127eb951f43
«Vitaly» <vitalik.yakhimovich@mail.ru>
2023-03-20 22:09:45 +0300

    add Exemple 003

---
+++ Example_003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает 2 числа 
+// и проверяет, является ли первое число квадратом второго
+
+Console.WriteLine("Введите первое целое число:");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число:");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA == numberB * numberB)
+{
+    Console.WriteLine($"Число {numberA} является квадратом второго числа");
+}
+else
+{
+    Console.WriteLine($"Число {numberA} не является квадратом второго числа");
+}
